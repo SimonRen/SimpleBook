20851b225f22921605bff4612451349a4ff1038a
jmulro <jmulro@gmail.com>
2013-01-08 13:26:11 +0800

    add missing files

---
+++ SimpleBook/data/system/util/init.lua
@@ -0,0 +1,15 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: jmulro
+-- Date: 1/7/13
+-- Time: 11:29 PM
+-- To change this template use File | Settings | File Templates.
+--
+local old_print = print
+
+print = function( ... )
+    if CFG.NOPRINT then return end
+
+    old_print( ... )
+end
+
---
+++ SimpleBook/data/system/view/init.lua
@@ -0,0 +1,15 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: jmulro
+-- Date: 1/7/13
+-- Time: 11:29 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+ViewReset = function()
+    print( "View Reset" )
+end
+
+ViewShow = function()
+    print( "View Show" )
+end
